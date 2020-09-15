e3dc2dfadf2951d2736e7483abb0bb8771a48ede
subhasrinika <71312704+subhasrinika@users.noreply.github.com>
2020-09-15 16:52:29 +0530

    Create range.py

---
+++ range.py
@@ -0,0 +1,11 @@
+list1=[12,-7,5,64,-14]
+print("Output")
+for num in list1:
+    if num>=0:
+        print(num,end="  ")
+    
+list2=[12,14,-95,3]
+print("\nOutput")
+for num in list2:
+    if num>=0:
+        print(num,end=" ")
