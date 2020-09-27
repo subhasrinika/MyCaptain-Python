6d17fe0e713894c93c6032e57ef811d25a1335ae
subhasrinika <71312704+subhasrinika@users.noreply.github.com>
2020-09-27 19:56:24 +0530

    Create Most_Frequent.py

---
+++ Most_Frequent.py
@@ -0,0 +1,13 @@
+def most_frequent(str1):
+    dict = {}
+    for n in str1:
+        keys = dict.keys()
+        if n in keys:
+            dict[n] += 1
+        else:
+            dict[n] = 1
+    return dict
+
+str1=input("Enter a string:")
+result=most_frequent(str1)
+print(result)
